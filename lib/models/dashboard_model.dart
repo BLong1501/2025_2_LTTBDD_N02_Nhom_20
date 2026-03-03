c6bbf8880e5d92ee3b2c7ee58fda14d7909941b5
Vu Thi Khanh Van <23010679@st.phenikaa-uni.edu.vn>
2026-03-03 13:18:43 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/models/dashboard_model.dart
    	modified:   lib/providers/admin_user_provider.dart
    	modified:   lib/services/admin_dashboard_service.dart
    	modified:   lib/view/admin/admin_home_screen.dart
    	modified:   lib/view/admin/manage_users_screen.dart

--- lib/models/dashboard_model.dart
+++ lib/models/dashboard_model.dart
@@ -5,27 +5,29 @@ class DashboardModel {
   final int featuredFoods;
 
   // Biểu đồ theo 12 tháng
-  final List<int> monthlyPending;   
-  final List<int> monthlyApproved; 
+  final List<int> monthlyUsers;
+  final List<int> monthlyRecipes;
+  final List<int> monthlyBloggerPosts;
 
   DashboardModel({
     required this.totalUsers,
     required this.totalFoods,
     required this.pendingFoods,
     required this.featuredFoods,
-    required this.monthlyPending,
-    required this.monthlyApproved,
+    required this.monthlyUsers,
+    required this.monthlyRecipes,
+    required this.monthlyBloggerPosts,
   });
 
-  /// Factory mặc định (tránh null)
   factory DashboardModel.empty() {
     return DashboardModel(
       totalUsers: 0,
       totalFoods: 0,
       pendingFoods: 0,
       featuredFoods: 0,
-      monthlyPending: List.filled(12, 0),
-      monthlyApproved: List.filled(12, 0),
+      monthlyUsers: List.filled(12, 0),
+      monthlyRecipes: List.filled(12, 0),
+      monthlyBloggerPosts: List.filled(12, 0),
     );
   }
 }
